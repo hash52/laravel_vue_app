3e30957d3ce890b34ae837985c544f78687595a2
1935Hiro <espresso.spacewood@gmail.com>
2022-08-14 12:01:18 +0900

    tasksのseederの追加による変更

--- laravel-vue-app/database/migrations/2022_08_13_151704_create_tasks_table.php
+++ laravel-vue-app/database/migrations/2022_08_13_151704_create_tasks_table.php
@@ -15,7 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('folder_id')->unsigned()->nullable();;
+            $table->integer('folder_id')->unsigned()->nullable();
             $table->string('title', 100);
             $table->date('due_date');
             $table->integer('status')->default(1);
